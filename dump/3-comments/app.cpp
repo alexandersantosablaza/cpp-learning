ffa2e6f8cabc380b3980600420437c956a6fa1bd
Ablaza <alexander.ablaza@capgemini.com>
2022-09-20 13:53:27 +0800

    c++ comments syntax

---
+++ dump/3-comments/app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+  // one line comment
+  /*
+    the comment of 
+    multiple lines 
+    this helps to put comment 
+    in code to guide yourself
+    in doing the program and use version control like git 
+    and not comment it if its not related to code
+  */
+ // author: alexander s ablaza
+  return 0;
+}
