e016677159ab1394f8699ed3ed39d2adf014825d
Alexander S Ablaza <alexander.ablaza@capgemini.com>
2022-09-21 04:01:21 +0800

    add source

--- source/app.cpp
+++ source/app.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-
-/**
- * main app
- */
+#include <iomanip>
+/*main app*/
 int main()
 {
-  //STL - part of the c++ standard library of container types or collections
+
+  /******************BOOLEAN******************/
+  // true or false  that is logically 1 or 0
+
+  bool red{true};
+  bool green{false};
+  if (red)
+  {
+    std::cout << "stop!" << std::endl;
+  }
+  if (green)
+  {
+    std::cout << "go!" << std::endl; // this wont print
+  }
+
+  std::cout
+      << red
+      << " "
+      << green
+      << std::boolalpha
+      << std::endl
+      << "changed the value to alphabetical using boolalpha"
+      << std::endl
+      << red << " " << green
+      << std::endl
+      << "check the size of bool: "
+      << sizeof(bool) << " in byte"
+      << std::endl;
+
   return 0;
 }
