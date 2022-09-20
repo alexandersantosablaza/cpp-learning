f14705b7e016f5726b192673f346ccd47617be48
Ablaza <alexander.ablaza@capgemini.com>
2022-09-21 03:49:28 +0800

    Boolean types

--- dump/7-data-type-and-lectures/app.cpp
+++ dump/7-data-type-and-lectures/app.cpp
@@ -246,6 +246,34 @@ int main()
   result = num11 / num12;
   std::cout << num11 << "/" << num12 << " = " << result << std::endl; // 0/0 = nan
 
+  /******************BOOLEAN******************/
+  // true or false  that is logically 1 or 0
+
+  bool red{true};
+  bool green{false};
+  if (red)
+  {
+    std::cout << "stop!" << std::endl;
+  }
+  if (green)
+  {
+    std::cout << "go!" << std::endl; // this wont print
+  }
+
+  std::cout
+      << red
+      << " "
+      << green
+      << std::boolalpha
+      << std::endl
+      << "changed the value to alphabetical using boolalpha"
+      << std::endl
+      << red << " " << green
+      << std::endl
+      << "check the size of bool: "
+      << sizeof(bool) << " in byte"
+      << std::endl;
+
   /******************STRINGS******************/
 
   const std::string letter = {};
