c71d17415620086f83922e91e338b1d747b9c41e
Ablaza <alexander.ablaza@capgemini.com>
2022-09-20 15:21:12 +0800

    git ignore

--- source/app.cpp
+++ source/app.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
+#include <iostream>
 
 /**
  * main app
  */
-int main() {
+int main()
+{
+  //STL - part of the c++ standard library of container types or collections
   return 0;
 }
