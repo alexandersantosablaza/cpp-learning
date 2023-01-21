2af09b7f894e0447dcb5b7a4881d37f00844bdff
Alexander_Ablaza <alexanderxablaza@gmail.com>
2023-01-22 06:22:05 +0800

    switch

---
+++ main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+int main(void)
+{
+  int val{};
+  switch (val)
+  {
+  case 1:
+    std::cout << "case 1\n";
+    break;
+  case 2:
+    std::cout << "case 2\n";
+    break;
+  case 3:
+    std::cout << "case 3\n";
+    break;
+  case 4:
+    std::cout << "case 4\n";
+    break;
+  }
+  return 0;
+}
