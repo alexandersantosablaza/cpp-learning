778139364abd28ec2cec7c5743100ab57fd9bee0
Alexander_Ablaza <alexanderxablaza@gmail.com>
2023-01-24 01:56:17 +0800

    members

--- main.cc
+++ main.cc
@@ -1,38 +1,6 @@
 #include <iostream>
-
-class Base
-{
-private:
-public:
-  int x;
-  static int y;
-
-public:
-  Base()
-  {
-    x = 0;
-    std::cout << "base(" << x << "," << y << ")\n";
-  }
-  Base(int x) : x{x}
-  {
-    std::cout << "base(" << x << "," << y << ")\n";
-  }
-  void print()
-  {
-    std::cout << "base(" << x << "," << y << ")\n";
-  }
-};
-int Base::y;
 int main(void)
 {
-
-  Base b;
-  b.x = 10;
-  Base b2;
-  b2.x = 20;
-  Base::y = 30;
-  b.print();
-  b2.print();
-
+  std::cout << "Hello world";
   return 0;
 }
