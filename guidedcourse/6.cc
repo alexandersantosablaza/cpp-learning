9ab6c7f9a7ac3edeeb5d3f1cb1e481fa18cecebe
Alexander_Ablaza <alexanderxablaza@gmail.com>
2023-01-24 02:58:33 +0800

    header files understanding

---
+++ guidedcourse/6.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "6.hh"
+int main(void)
+{
+
+  std::cout << product(4, 5);
+  return 0;
+}
---
+++ guidedcourse/6.hh
@@ -0,0 +1,5 @@
+// remember to name your header file to its purpose this is a header with product function!
+int product(int a = 0, int b = 0)
+{
+  return a * b;
+}
